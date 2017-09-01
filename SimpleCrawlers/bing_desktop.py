cab5688870c8e7624d22cf1015ee422bdc8fa80d
sid <sidzhang@zhaoxin.com>
2017-09-01 18:42:41 +0800

    sycn

--- SimpleCrawlers/bing_desktop.py
+++ SimpleCrawlers/bing_desktop.py
@@ -1,9 +1,12 @@
 
 import requests
+import win32api
 
 def main():
-    r = requests.get(r"http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US")
-    print(r.text)
+    print(win32api.GetSystemMetrics(0))
+    print(win32api.GetSystemMetrics(1))
+    #r = requests.get(r"http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US")
+    #print(r.text)
 
 
 if __name__ == "__main__":
